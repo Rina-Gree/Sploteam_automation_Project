6ccc43472b7c1582adef9e7fa754b824e5c36a37
Rina <rinagreesh@gmail.com>
2025-03-31 23:16:52 +0300

    update tests

--- src/test/java/com/juneit/Pages/EventsPage.java
+++ src/test/java/com/juneit/Pages/EventsPage.java
@@ -12,15 +12,16 @@ import  java.util.List;
 
 
 public class EventsPage {
-    private WebDriver driver;
+    private final WebDriver driver;
+
     public EventsPage(WebDriver driver) {
         this.driver = driver;
     }
+
     public List<WebElement> getEventsAndCreateGamesTabs() {
         List<WebElement> eventsAndCreateGamesTabs = driver.findElements(By.className(EVENTS_AND_CREATE_GAMES_TABS_CLASS));
         return eventsAndCreateGamesTabs;
     }
-
     public WebElement getFirstDate() {
         WebElement getFirstDate = driver.findElement(By.xpath(FIRST_DATE_XPATH));
         return getFirstDate;
@@ -29,22 +30,18 @@ public class EventsPage {
         WebElement getDropdownArena = driver.findElement(By.xpath(EVENT_DROPDOWN_ARENA_XPATH));
         return getDropdownArena;
     }
-
     public WebElement changeArenaName() {
         WebElement changeArenaName = driver.findElement(By.xpath(ARENA_NAME_XPATH));
         return changeArenaName;
     }
-
     public WebElement ChangeTimeForGame() {
         WebElement ChangeTimeForGame = driver.findElement(By.xpath(TIME_FOR_GAME_XPATH));
         return ChangeTimeForGame;
     }
-
     public WebElement getChangeGamesButton() {
         WebElement getChangeGamesButton = driver.findElement(By.xpath(CREATE_GAME_BUTTON_XPATH));
         return getChangeGamesButton;
     }
-
     public WebElement getLoginWindow() {
         WebElement getLoginWindow = driver.findElement(By.xpath(MODAL_WINDOW_LOGIN_XPATH));
         return getLoginWindow;
@@ -57,16 +54,16 @@ public class EventsPage {
         WebElement getCityTverChange = driver.findElement(By.xpath(CHANGE_CITY_TVER_XPATH));
         return getCityTverChange;
     }
-        public WebElement getTimeSlotsForCreateGames() {
-            WebElement getTimeSlotsForCreateGames = driver.findElement(By.className(CREATE_GAME_TIME_CARDS));
-            return getTimeSlotsForCreateGames;
+    public WebElement getTimeSlotsForCreateGames() {
+        WebElement getTimeSlotsForCreateGames = driver.findElement(By.className(CREATE_GAME_TIME_CARDS));
+        return getTimeSlotsForCreateGames;
     }
     public List<WebElement> getCalendarForCreateGame() {
         List<WebElement> getCalendarForCreateGame = driver.findElements(By.className(CALENDAR_CLASS));
         return getCalendarForCreateGame;
     }
     public WebElement getCardsForCreateGames() {
-        WebElement getCardsForCreateGames= driver.findElement(By.className(EVENT_GAME_CREATE_CARDS_XPATH));
+        WebElement getCardsForCreateGames = driver.findElement(By.className(EVENT_GAME_CREATE_CARDS_XPATH));
         return getCardsForCreateGames;
     }
     public WebElement getTextIfNotCardsForGames() {
@@ -85,36 +82,36 @@ public class EventsPage {
         WebElement getAssertBeginTimeForEveryCard = driver.findElement(By.className(TIME_EVERY_CARD_XPATH));
         return getAssertBeginTimeForEveryCard;
     }
+    public List<WebElement> getDateOfCalendar() {
+        List<WebElement> getDateOfCalendar = driver.findElements(By.className(CALENDAR_DATE_CLASS));
+        return getDateOfCalendar;
+    }
 
+        public static final String EVENTS_AND_CREATE_GAMES_TABS_CLASS = "Tabs_tab__3e8GV";
+        public static final String EVENT_DROPDOWN_ARENA_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[3]/div[1]/div[1]";
 
+        // CreateGameIfNotLogin
+        public static final String FIRST_DATE_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[1]/div/div[2]/span[2]";
+        public static final String ARENA_NAME_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[3]/div[1]/div[1]/div[2]/div[2]";
+        public static final String TIME_FOR_GAME_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[4]/div/div/div[2]/div[1]/div[1]";
+        public static final String CREATE_GAME_BUTTON_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[4]/div/div/div[3]/div[4]/button";
+        public static final String MODAL_WINDOW_LOGIN_XPATH = "/html/body/div[3]/div/div";
 
-    public static final String EVENTS_AND_CREATE_GAMES_TABS_CLASS = "Tabs_tab__3e8GV";
-    public static final String EVENT_DROPDOWN_ARENA_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[3]/div[1]/div[1]";
+        //CreateGameForTver
+        public static final String CHANGE_CITY_XPATH = "//*[@id=\"root\"]/header/div/div/div[1]/div[1]";
+        public static final String CHANGE_CITY_TVER_XPATH = "//*[@id=\"root\"]/header/div/div/div[1]/div[3]/div[44]";
 
-    // CreateGameIfNotLogin
-    public static final String FIRST_DATE_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[1]/div/div[2]/span[2]";
-    public static final String ARENA_NAME_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[3]/div[1]/div[1]/div[2]/div[2]";
-    public static final String TIME_FOR_GAME_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[4]/div/div/div[2]/div[1]/div[1]";
-    public static final String CREATE_GAME_BUTTON_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[4]/div/div/div[3]/div[4]/button";
-    public static final String MODAL_WINDOW_LOGIN_XPATH = "/html/body/div[3]/div/div";
+        public static final String CALENDAR_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[1]/div/div[4]";
+        public static final String CALENDAR_CLASS = "NavLink_navLink__3OIyY";
+        public static final String CREATE_GAME_TIME_CARDS = "Tabs_tabPanel__N-v3z";
+        public static final String EVENT_GAME_CREATE_CARDS_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[4]/div";
+        public static final String TEXT_IF_EMPTY_CARDS_GAME_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[4]/div";
+        public static final String FIRST_DATE_CHANGE_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[1]/div/div[1]/span[2]";
+        public static final String TIME_EVERY_CARD_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[4]/div/div/div[2]/div[2]/div[1]";
 
-     //CreateGameForTver
-     public static final String CHANGE_CITY_XPATH = "//*[@id=\"root\"]/header/div/div/div[1]/div[1]";
-    public static final String CHANGE_CITY_TVER_XPATH = "//*[@id=\"root\"]/header/div/div/div[1]/div[3]/div[44]";
+        public static final String BEGIN_AND_END_TIME_CLASS = "create-event__time";
+        public static final String BEGIN_AND_END_TIME_CLASS = "create-event__time";
+        public static final String CALENDAR_DATE_CLASS = "NavLink_navLink__3OIyY";
 
-    public static final String CALENDAR_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[1]/div/div[4]";
-    public static final String CALENDAR_CLASS = "NavLink_navLink__3OIyY";
-    public static final String CREATE_GAME_TIME_CARDS = "Tabs_tabPanel__N-v3z";
-
-
-    public static final String EVENT_GAME_CREATE_CARDS_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[4]/div";
-
-    public static final String TEXT_IF_EMPTY_CARDS_GAME_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[4]/div";
-    public static final String FIRST_DATE_CHANGE_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[1]/div/div[1]/span[2]";
-    public static final String TIME_EVERY_CARD_XPATH = "//*[@id=\"root\"]/div[2]/div[1]/div/div[4]/div/div/div[2]/div[2]/div[1]";
-
-    public static final String BEGIN_AND_END_TIME_CLASS = "create-event__time";
-
-
-
+    }
 }
--- src/test/java/com/juneit/Pages/MainPage.java
+++ src/test/java/com/juneit/Pages/MainPage.java
@@ -18,17 +18,14 @@ public class MainPage {
         WebElement signInButton = driver.findElement(By.className(SIGNIN_BUTTON_CLASS));
         return signInButton;
     }
-
     public WebElement getLogoHeader(){
         WebElement logoHeader = driver.findElement(By.className(LOGO_HEADER_SPLOTEAM_CLASS));
         return logoHeader;
     }
-
     public WebElement getMoreGamesButton(){
         WebElement moreGamesButton = driver.findElement(By.xpath(SHOW_MORE_GAMES_BUTTON_XPATH));
         return moreGamesButton;
     }
-
     public WebElement getCreateGameButton(){
         WebElement moreGamesButton = driver.findElement(By.xpath(CREATE_GAME_BUTTON_XPATH));
         return moreGamesButton;}
@@ -38,12 +35,10 @@ public class MainPage {
         String userName = getUserName().getText();
         assertEquals("Irina_test", userName);
         return getUserName;
-
     }
     public WebElement getLogoNotification(){
         WebElement getLogoNotification = driver.findElement(By.xpath(LOGO_NOTIFICATION_XPATH));
         return getLogoNotification;}
-
     public WebElement getCountNotification(){
         WebElement getCountNotification = driver.findElement(By.className(COUNT_NOTIFICATION_CLASS));
         return getCountNotification;}
@@ -51,12 +46,18 @@ public class MainPage {
         WebElement userNameHeader = driver.findElement(By.className(USER_NAME_HEADER_CLASS));
         return userNameHeader;
     }
-
     public WebElement getCampImage(){
         WebElement CampImage = driver.findElement(By.className(CAMP_IMAGE_CLASS));
         return CampImage;
     }
-
+    public WebElement getLocalTime(){
+        WebElement getLocalTime = driver.findElement(By.className(LOCAL_TIME_CLASS));
+        return getLocalTime;
+    }
+    public WebElement getFooterInfo(){
+        WebElement getFooterInfo = driver.findElement(By.className(FOOTER_DATE_CLASS));
+        return getFooterInfo;
+    }
 
 
 
@@ -65,14 +66,11 @@ public class MainPage {
     public static final String SHOW_MORE_GAMES_BUTTON_XPATH = "//*[@id=\"root\"]/div[2]/section[2]/div/div[3]/a";
     public static final String COMING_GAMES_TEXT_CLASS = "coming-games__text";
     public static final String CREATE_GAME_BUTTON_XPATH = "//*[@id=\"root\"]/div[2]/section[3]/div/div[2]/a";
-
     public static final String USER_NAME_XPATH = "//*[@id=\"root\"]/header/div/div/div[3]/div[2]/div[1]";
     public static final String LOGO_NOTIFICATION_XPATH = "//*[@id=\"root\"]/header/div/div/div[2]/div[1]/img";
-
     public static final String COUNT_NOTIFICATION_CLASS = "Widget_label__3nbvW";
-
     public static final String USER_NAME_HEADER_CLASS =  "profileText__name";
-
     public static final String CAMP_IMAGE_CLASS = "CampsButton_img__3CoXw";
-
-}
+    public static final String LOCAL_TIME_CLASS = "locationTimezone";
+    public static final String FOOTER_DATE_CLASS ="footer__copyright";
+    }
